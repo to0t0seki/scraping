3407bc3479a20fb3e4b37e345291f7169e18a50f
EC2 Default User <ec2-user@ip-172-31-24-95.ap-northeast-1.compute.internal>
2020-08-10 16:45:15 +0000

    3 commit

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -1,23 +1,37 @@
+require "date"
+
 class ScrapingController < ApplicationController
     def index
+        @today = Date.today
     end
     
     def jag
+       
+  
+                #  binding.pry
         categries = ["81/1","1/0","2/0","6/0"]
         @jag = {}
         @total = []
+        @ave = []
         agent = Mechanize.new
         
         for category in categries do
             tmp = {}
             total = 0
-            current_page = agent.get("https://papimo.jp/h/00041817/hit/index_sort/215060005/1-20-279176/" + category)
+            
+            begin  
+            current_page = agent.get("https://papimo.jp/h/"+params[:hall]+"/hit/index_sort/"+params[:machine]+"/1-20-279176/" + category)
+            rescue WWW::Mechanize::ResponseCodeError => e
+                puts error
+                  binding.pry
+            end
+             
             elements = current_page.search("#table-sort tbody tr")
             
             elements.each do |ele|
                 td = ele.search("td")
-                tmp[td[0].inner_text] = td[2].inner_text.gsub(",","")
-                total += td[2].inner_text.gsub(",","").to_i
+                tmp[td[0].inner_text] = td[params[:date].to_i + 1].inner_text.gsub(",","")
+                total += td[params[:date].to_i + 1].inner_text.gsub(",","").to_i
             end
             @total << total
             
@@ -40,8 +54,24 @@ class ScrapingController < ApplicationController
             value << count.round(2)
             @jag[key] = value
         end
-        puts @total
+        # binding.pry
+        begin
+            @ave << @total[3] / @total[1]
+        rescue ZeroDivisionError
+            @ave << 0
+        end
         
+        begin    
+            @ave << @total[3] / @total[2]
+        rescue ZeroDivisionError
+            @ave << 0
+        end
+        
+        begin
+            @ave << (@total[3].to_i.to_f / ((@total[1].to_i * 312 +  + @total[2].to_i * 104) - @total[0].to_i) * 50).round(2)
+        rescue ZeroDivisionError
+            @ave << 0
+        end
         
     end
 end
